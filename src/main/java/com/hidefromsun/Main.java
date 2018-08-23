e070b2ae1d795d90c7064093589b7f439d9fbcd6
dm-kovalev <dmitry.kovalev-m@ya.ru>
2018-08-24 02:10:30 +0300

    Naive computing implementation

--- src/main/java/com/hidefromsun/Main.java
+++ src/main/java/com/hidefromsun/Main.java
@@ -5,6 +5,7 @@ import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -15,11 +16,26 @@ import com.google.maps.internal.PolylineEncoding;
 import com.google.maps.model.DirectionsResult;
 import com.google.maps.model.DirectionsRoute;
 import com.google.maps.model.LatLng;
+import net.e175.klaus.solarpositioning.AzimuthZenithAngle;
+import net.e175.klaus.solarpositioning.DeltaT;
+import net.e175.klaus.solarpositioning.Grena3;
 
 /**
  * @author abulgako
  */
 public class Main {
+
+    // https://www.movable-type.co.uk/scripts/latlong.html
+    private static double computeBearing(LatLng src, LatLng dst) {
+        double dLng = (dst.lng - src.lng);
+
+        double y = Math.sin(dLng) * Math.cos(dst.lat);
+        double x = Math.cos(src.lat) * Math.sin(dst.lat) - Math.sin(src.lat)
+                * Math.cos(dst.lat) * Math.cos(dLng);
+
+        double bearing = Math.toDegrees(Math.atan2(y, x));
+        return (bearing + 360) % 360;
+    }
 
     public static void main(String[] args) throws InterruptedException, ApiException, IOException {
         GeoApiContext context = new GeoApiContext.Builder()
@@ -28,6 +44,7 @@ public class Main {
         DirectionsResult results = DirectionsApi.getDirections(context, "Peterhof", "Saint-Petersburg")
                 .alternatives(true)
                 .await();
+        context.shutdown();
 
         //results.routes[0].legs[0].steps[0].polyline.decodePath()
         List<List<LatLng>> routes = new ArrayList<>();
@@ -43,7 +60,34 @@ public class Main {
             routes.add(lngs);
         }
 
-        System.out.println();
-    }
+        int sunOnTheRight = 0;
 
+        // just for example
+        List<LatLng> route = routes.get(0);
+        for (int i = 0; i < route.size() - 2; i++) {
+            GregorianCalendar calendar = new GregorianCalendar(2018, 8, 23, 16, 0);
+            LatLng currentPosition = route.get(i);
+
+            double bearing = computeBearing(currentPosition, route.get(i + 1));
+            AzimuthZenithAngle sunPosition = Grena3.calculateSolarPosition(
+                    calendar,
+                    currentPosition.lat,
+                    currentPosition.lng,
+                    DeltaT.estimate(calendar));
+            double sunAzimuth = sunPosition.getAzimuth();
+
+            // if they are in the same half-plane
+            if (bearing <= 180 && sunAzimuth <= 180 || bearing > 180 && sunAzimuth > 180) {
+                if (bearing < sunAzimuth)
+                    sunOnTheRight++;
+            }
+            else {
+                if (bearing > (sunAzimuth + 180) % 360)
+                    sunOnTheRight++;
+            }
+        }
+
+        String result = sunOnTheRight > route.size() / 2 ? "left" : "right";
+        System.out.println(String.format("Sit on the %s side", result));
+    }
 }
